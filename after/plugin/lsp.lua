b5e8b8f51a3b50b8f1d113205e579f16ee60e79c
tsukiongithub <lucasmoon0@gmail.com>
2023-03-16 16:13:43 +0100

    initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+	"tsserver",
+	"eslint",
+	"sumneko_lua"
+})
+
+lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
