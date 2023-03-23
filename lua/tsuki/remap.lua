cf0191cbfc27bf4e63e4781c97a8adb16024b97b
tsuki <tsuki@pop-os.localdomain>
2023-03-23 10:25:53 +0100

    added quick buffer navigation. <leader>n :bnext. <leader>N :bprevious

--- lua/tsuki/remap.lua
+++ lua/tsuki/remap.lua
@@ -1,7 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<cr>")
-vim.keymap.set("n", "<leader>sv", ":source $MYVIMRC | echo 'vimrc reloaded'<cr>")
+vim.keymap.set("n", "<leader>sv", ":so $MYVIMRC | echo 'vimrc reloaded'<cr>")
 vim.keymap.set("n", "<leader>f", "<cmd>Format<cr>")
+
+vim.keymap.set("n", "<leader>N", ":bprevious<cr>")
+vim.keymap.set("n", "<leader>n", ":bnext<cr>")
 
 require('nvim_comment').setup({
   line_mapping = "<leader>cl",
